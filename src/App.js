5c09d4ea483d6db16e7cca4b044ecc7fc1f79bee
Rob <robertaguilerajr@gmail.com>
2017-07-23 22:29:43 -0700

    Axios & React Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./component/Header";
+import SideBar from "./component/SideBar";
+import ChatPanel from "./component/ChatPanel";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    let res = axios.get("http://localhost:8887/newMessages").then(res => {
+      console.log(res);
+    });
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <SideBar />
+        <ChatPanel />
       </div>
     );
   }
---
+++ src/component/ChatPanel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatPanel = () => {
+  return <div />;
+};
+
+export default ChatPanel;
